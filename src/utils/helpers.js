dc49a321b7dbb0af9615aa1faaaf4430d3bd71ad
Farell Aditya Ardian <farell@rerechanstore.eu.org>
2025-08-27 00:37:40 +0700

    Create helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,25 @@
+// src/utils/helpers.js
+module.exports = {
+  formatRupiah: (number) => {
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0,
+    }).format(number || 0);
+  },
+  backButton: (text = '⬅️ Kembali', callback_data = 'back_menu') => {
+    return { text, callback_data };
+  },
+  prettyLine: () => '------------------------------------------',
+  censorUsername: (username) => {
+    if (!username || username.length < 3) return '***';
+    const start = username.slice(0, 2);
+    const end = username.slice(-1);
+    const censored = '*'.repeat(Math.max(3, username.length - 3));
+    return `${start}${censored}${end}`;
+  },
+  censorBalance: (amount) => {
+      const formatted = module.exports.formatRupiah(amount);
+      return formatted.replace(/\d/g, (match, offset) => (offset < 4 ? match : '*'));
+  }
+};
